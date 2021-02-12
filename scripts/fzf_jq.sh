ec39eaebf3dd9e4a21a3144013b132a6dd2a3837
Belden Lyman <belden@retailnext.net>
2021-02-12 14:37:47 -0800

    Add --help option

--- scripts/fzf_jq.sh
+++ scripts/fzf_jq.sh
@@ -3,11 +3,52 @@
 jq_color_opt=""
 json_document=
 
-while getopts c-: opt; do
+function usage() {
+  local exit_val=${1:-0}
+cat <<'.'
+NAME
+  fzf_jq.sh - a toy REPL for `jq` built on `fzf`
+
+SYNOPSIS
+
+  fzf_jq.sh [-c | --color | --colour ] [ <FILE> ]
+
+DESCRIPTION
+
+  This is a toy REPL for `jq`. When run without any arguments, you will begin exploring the JSON document:
+
+    ```js
+    {
+        "hello": "world"
+    }
+    ```
+
+
+  You may specify your own file to explore by providing its path on the command line.
+
+  By default, `jq` disables colorized output when run non-interactively. You may force it on by providing any of the colorization options.
+
+OPTIONS
+
+    -c            --   Force `jq`'s output to be colorized.
+    --color
+    --colour
+
+EXAMPLES
+
+    $ fzf_jq.sh --color /tmp/some-big-file.json
+.
+  exit $exit_val
+}
+
+while getopts ch-: opt; do
   case $opt in
     c )
       jq_color_opt=-C
       shift
+      ;;
+    h )
+      usage
       ;;
     - )
       long_opt=$OPTARG
@@ -16,11 +57,16 @@ while getopts c-: opt; do
           jq_color_opt=-C
           shift
           ;;
+        h | help )
+          usage
+          ;;
         * )
+          usage 1
           ;;
       esac
       ;;
     * )
+      usage 1
       ;;
   esac
 done
