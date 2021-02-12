e8838e8fc58da61053071e3c672534fbb14f1f6b
Belden Lyman <belden@retailnext.net>
2021-02-12 14:26:06 -0800

    Allow user to specify their own json document

--- scripts/fzf_jq.sh
+++ scripts/fzf_jq.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
 jq_color_opt=""
+json_document=
 
 while getopts c-: opt; do
   case $opt in
     c )
       jq_color_opt=-C
+      shift
       ;;
     - )
       long_opt=$OPTARG
       case $long_opt in
         c | color | colour )
           jq_color_opt=-C
+          shift
           ;;
         * )
           ;;
@@ -22,4 +25,14 @@ while getopts c-: opt; do
   esac
 done
 
-echo '' | fzf --print-query --preview "echo -e '{\n  \x22hello\x22: \x22world\x22\n}' | jq ${jq_color_opt} {q}"
+if [[ ${#*} -ge 1 ]]; then
+  json_document="$1"
+  shift
+fi
+
+if [[ -z "$json_document" ]]; then
+  echo '' | fzf --print-query --preview "echo -e '{\n  \x22hello\x22: \x22world\x22\n}' | jq ${jq_color_opt} {q}"
+else
+  echo '' | fzf --print-query --preview "jq ${jq_color_opt} {q} ${json_document}"
+fi
+
