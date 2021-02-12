8110af04f741ccb7eb27d9a944da8a0c5ad16474
Belden Lyman <belden@retailnext.net>
2021-02-12 14:12:22 -0800

    Add support for colorized jq
    Any of these:
    
    ```sh
    $ ./fzf_jq.sh -c
    $ ./fzf_jq.sh --c
    $ ./fzf_jq.sh --color
    $ ./fzf_jq.sh --colour
    ```
    
    will now produce a colorized interaction with `jq`.

--- scripts/fzf_jq.sh
+++ scripts/fzf_jq.sh
@@ -1,3 +1,25 @@
 #!/usr/bin/env bash
 
-echo '' | fzf --print-query --preview 'echo -e "{\n  \x22hello\x22: \x22world\x22\n}" | jq {q}'
+jq_color_opt=""
+
+while getopts c-: opt; do
+  case $opt in
+    c )
+      jq_color_opt=-C
+      ;;
+    - )
+      long_opt=$OPTARG
+      case $long_opt in
+        c | color | colour )
+          jq_color_opt=-C
+          ;;
+        * )
+          ;;
+      esac
+      ;;
+    * )
+      ;;
+  esac
+done
+
+echo '' | fzf --print-query --preview "echo -e '{\n  \x22hello\x22: \x22world\x22\n}' | jq ${jq_color_opt} {q}"
